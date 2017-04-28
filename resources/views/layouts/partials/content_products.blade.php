82d69e89c272ad6a3a02c5de7401cbcd621f3552
Alonso Cauich <alonsocauichv1@gmail.com>
2017-04-28 14:26:01 -0600

    Productos
    Se añadio apartado de el portafolio de productos dandole efectos de
    traslacióna cada item.

--- resources/views/layouts/partials/content_products.blade.php
+++ resources/views/layouts/partials/content_products.blade.php
@@ -6,9 +6,53 @@
       </div>
       <div class="row">
           <div class="features">
-              <div class="col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure style="text-align:center">
+                <img src="img/tecnologia/tec01.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure>
+                <img src="img/tecnologia/tec02.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure>
+                <img src="img/tecnologia/tec03.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
 
-              </div><!--/.col-md-4-->
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure>
+                <img src="img/tecnologia/tec04.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure>
+                <img src="img/tecnologia/tec05.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure>
+                <img src="img/tecnologia/tec06.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
+
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure>
+                <img src="img/tecnologia/tec07.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure>
+                <img src="img/tecnologia/tec08.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
+            <div class="col-md-4 col-sm-6 wow fadeInUp portfolio-item-product padding-right-zero mr-btn-15" data-wow-duration="300ms" data-wow-delay="0ms">
+              <figure>
+                <img src="img/tecnologia/tec09.png" class="img-responsive">
+              </figure>
+            </div><!--/.col-md-4-->
           </div>
       </div><!--/.row-->
   </div><!--/.container-->
